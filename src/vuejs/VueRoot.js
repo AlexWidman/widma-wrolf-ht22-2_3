f1f81c06b0d05e9d955661f08ec55a7bade131c9
William Rolf <wrolf@kth.se>
2022-11-21 12:04:06 +0100

    Almost done with TW3.5

--- src/vuejs/VueRoot.js
+++ src/vuejs/VueRoot.js
@@ -1,6 +1,24 @@
 // Add relevant imports here 
+import { firebaseModelPromise, updateFirebaseFromModel, updateModelFromFirebase } from "../firebaseModel";
+import resolvePromise from "../resolvePromise";
+import promiseNoData from "../views/promiseNoData";
 
 // Define the VueRoot component
-// const VueRoot = {TODO}
-
+const VueRoot = { 
+                data() { return {promiseState: {}}; }, 
+                created() {
+                    function connectToFirebaseACB() { 
+                        if (!this.promiseState.data) return;
+                        updateFirebaseFromModel(this.promiseState);
+                        updateModelFromFirebase(this.promiseState);
+                    }
+                    if (!this.promiseState.promise) {
+                        resolvePromise(firebaseModelPromise(), this.promiseState, connectToFirebaseACB)
+                    }
+                    },
+                render(){
+                    return promiseNoData(this.promiseState) || <App model={this.promiseState.data} />;
+                    },
+                };
 // Export the VueRoot component
+export default VueRoot;
