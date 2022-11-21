37c7a066f42b77d685d3b9a470aae4fcdc901941
William Rolf <wrolf@kth.se>
2022-11-21 12:03:10 +0100

    Almost done with TW3.5

--- src/firebaseModel.js
+++ src/firebaseModel.js
@@ -1,29 +1,83 @@
 // Add relevant imports here 
-// TODO
+import DinnerModel from "./DinnerModel.js";
+import { getDishDetails } from "./dishSource.js";
+import firebaseConfig from "./firebaseConfig.js";
+import resolvePromise from "./resolvePromise.js";
 
 // Initialise firebase
-// TODO
+firebase.initializeApp(firebaseConfig);
+const REF="dinnerModel123";
 
-function observerRecap(/*TODO*/) {
-    //TODO
+function observerRecap(model) {
+    function checkPayload(payload) {
+        console.log(payload)
+    }
+    model.addObserver(checkPayload)
 }
 
 function firebaseModelPromise() {
     function makeBigPromiseACB(firebaseData) {
-        //TODO
+        if (!firebaseData.val() || Object.keys(firebaseData.val()).length === 0) { return new DinnerModel(); }
+        const dishPromiseArray= Object.keys(firebaseData.val().dishes).map(makeDishPromiseCB);
+        function createModelACB(dishArray){
+            return new DinnerModel(firebaseData.val().numberOfGuests, dishArray);
+        }
+        return Promise.all(dishPromiseArray).then(createModelACB);
     }
-    return;
+    function makeDishPromiseCB(dishId){
+        return getDishDetails(dishId);
+    }
+    return firebase.database().ref(REF /* <-- note! Whole object! */).once("value").then(makeBigPromiseACB);
 }
 
 function updateFirebaseFromModel(model) {
-    //TODO
+    function observePayloadACB(payload) {
+        if (!payload) { return; }
+        
+        if (payload.setGuests) {
+            firebase.database().ref(REF+"/numberOfGuests").set(model.numberOfGuests)
+        }
+        else if(payload.dishID){
+            firebase.database().ref(REF+"/currentDish").set(model.currentDish)    
+        }
+        else if (payload.addDish) {
+            firebase.database().ref(REF+"/dishes/"+ payload.addDish.id).set(payload.addDish.title)
+        }
+        else if (payload.removeDish) {
+            firebase.database().ref(REF+"/dishes/"+ payload.removeDish.id).set(null)
+        }    
+    }
+    model.addObserver(observePayloadACB)
     return;
 }
+console.log(updateFirebaseFromModel)
 
 function updateModelFromFirebase(model) {
-    //TODO
-    return;
+    function guestsChangedInFirebaseACB(firebaseData){ model.setNumberOfGuests(firebaseData.val());}
+    firebase.database().ref(REF+"/numberOfGuests").on("value", guestsChangedInFirebaseACB);
+    
+    function dishChangedInFirebaseACB(firebaseData){ model.setCurrentDish(firebaseData.val());}
+    firebase.database().ref(REF+"/currentDish").on("value", dishChangedInFirebaseACB);
+
+    function dishAddedInFirebaseACB(firebaseData){
+        function responseDishDataACB(dish) {
+            model.addToMenu(dish);
+        }
+        function fetchDishDataBasedOnID(dishId) {
+            function checkDishDuplicateCB(dish) {
+                return dish.id === dishId;
+            }
+            return model.dishes.find(checkDishDuplicateCB);    
+        }
+        if (!fetchDishDataBasedOnID(+firebaseData.key)) {
+            getDishDetails(+firebaseData.key).then(responseDishDataACB);
+        }
+    }
+    firebase.database().ref(REF+"/dishes").on("child_added", dishAddedInFirebaseACB);
+
+    function dishRemovedInFirebaseACB(firebaseData){ model.removeFromMenu({ id: +firebaseData.key });}
+    firebase.database().ref(REF+"/dishes").on("child_removed",  dishRemovedInFirebaseACB);
 }
 
 // Remember to uncomment the following line:
-// export {observerRecap, firebaseModelPromise, updateFirebaseFromModel, updateModelFromFirebase};
+export {observerRecap, firebaseModelPromise, updateFirebaseFromModel, updateModelFromFirebase};
