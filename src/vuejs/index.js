2491ce0b504464fe4a6ac4e32debc771c427330d
Alexander Widman <widma@kth.se>
2022-11-22 16:54:24 +0100

    testing

--- src/vuejs/index.js
+++ src/vuejs/index.js
@@ -11,7 +11,7 @@ import "firebase/database";
 window.firebase=firebase;
 
 // TODO import navigation
-//import "../views/navigation.js";
+import "../views/navigation.js";
 
 // using require() instead of import, for the above assignments to take effect before VueRoot is loaded
 const VueRoot=require("/src/vuejs/VueRoot.js").default;
